d9b707590930846d6189559b74398fcc3e48e96f
Kiwi <agiannoulis@mavestro.co>
2021-06-30 17:37:35 +0300

    Update to the STATUS enum

--- template/src/module/start.go
+++ template/src/module/start.go
@@ -2,6 +2,6 @@ package module
 
 import module "github.com/ProjectAthenaa/sonic-core/protos"
 
-func (t Task) Start(data *module.Data) (updates chan *module.Status, commands chan *module.COMMAND, err error) {
+func (t Task) Start(data *module.Data) (updates chan *module.STATUS, commands chan *module.COMMAND, err error) {
 	panic("implement me")
 }
