8661fa094024fefe445a6f5d1389229a27538fbd
Kiwi <agiannoulis@mavestro.co>
2021-07-04 01:44:50 +0300

    Updated connection driver name

--- sonic/database/connect.go
+++ sonic/database/connect.go
@@ -3,7 +3,7 @@ package database
 import "github.com/ProjectAthenaa/sonic-core/sonic/database/ent"
 
 func Connect(pgURL string) *ent.Client {
-	client, err := ent.Open("postgresql", pgURL)
+	client, err := ent.Open("postgres", pgURL)
 	if err != nil {
 		panic(err)
 	}
