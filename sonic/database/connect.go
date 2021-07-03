e173235e33c50c172b695a9a74dbe803bbe8d413
Kiwi <agiannoulis@mavestro.co>
2021-07-04 02:00:37 +0300

    Added migration to the schema

--- sonic/database/connect.go
+++ sonic/database/connect.go
@@ -1,9 +1,18 @@
 package database
 
-import "github.com/ProjectAthenaa/sonic-core/sonic/database/ent"
+import (
+	"context"
+	"github.com/ProjectAthenaa/sonic-core/sonic/database/ent"
+	"github.com/ProjectAthenaa/sonic-core/sonic/database/ent/migrate"
+)
 
 func Connect(pgURL string) *ent.Client {
 	client, err := ent.Open("postgres", pgURL)
+	if err != nil {
+		panic(err)
+	}
+
+	err = client.Schema.Create(context.Background(), migrate.WithDropIndex(true), migrate.WithDropColumn(true))
 	if err != nil {
 		panic(err)
 	}
