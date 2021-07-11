3387e1bbb720b08138488f4ece6426f0030ac4cc
Kiwi <agiannoulis@mavestro.co>
2021-07-11 05:02:37 +0300

    Added method

---
+++ webhooks/webhooks.go
@@ -0,0 +1,12 @@
+package webhooks
+
+import "google.golang.org/grpc"
+
+func NewClient() WebhooksClient {
+	conn, err := grpc.Dial("webhook-service.general.svc.cluster.local:3000")
+	if err != nil {
+		panic(err)
+	}
+
+	return NewWebhooksClient(conn)
+}
