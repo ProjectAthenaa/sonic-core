901fae40ef0855cfaf9ac4c0c5844d04a4a22e41
Kiwi <agiannoulis@mavestro.co>
2021-08-07 23:59:47 +0300

    Removed import cycle

--- sonic/frame/redis.go
+++ sonic/frame/redis.go
@@ -2,21 +2,14 @@ package frame
 
 import (
 	"context"
-	"github.com/ProjectAthenaa/sonic-core/sonic/core"
 	"github.com/go-redis/redis/v8"
 	"os"
 )
 
-func init(){
-	_, err := core.Base.NewRedis("monitors", os.Getenv("REDIS_URL"))
-	if err != nil{
-		panic(err)
-	}
-}
+var rdb = ConnectRedis(os.Getenv("REDIS_URL"))
 
 //SubscribeToChannel connects to a redis pub/sub stream and returns a pointer to a PubSub struct
 func SubscribeToChannel(channelName string) (ps *PubSub, err error) {
-	rdb := core.Base.GetRedis("monitors")
 	if len(channelName) == 0 {
 		return nil, channelEmptyError
 	}
@@ -41,22 +34,6 @@ type PubSub struct {
 //Close closes the underlying pub/sub stream as well as the channel attached to it
 func (p *PubSub) Close() error {
 	return p.redisPS.Close()
-}
-
-//ConnectToRedis is an internal method used to create a redis client to use with pub/sub
-func ConnectToRedis() *redis.Client {
-	redisURL := os.Getenv("REDIS_URL")
-
-	if !redisURLRegex.MatchString(redisURL) {
-		return nil
-	}
-
-	opts, err := redis.ParseURL(os.Getenv("REDIS_URL"))
-	if err != nil {
-		return nil
-	}
-
-	return redis.NewClient(opts)
 }
 
 func ConnectRedis(dsn string) *redis.Client {
