2992751fee7f625a9ef89311d3a5a97253fe24c5
Kiwi <agiannoulis@mavestro.co>
2021-08-10 02:41:51 +0300

    GrabValueFromHTMLName

--- sonic/utils.go
+++ sonic/utils.go
@@ -93,6 +93,6 @@ func GetMonitorProxy(site Site) (proxy *url.URL, authorization string, err error
 	return url, base64.StdEncoding.EncodeToString([]byte("KJND3:5Z6GNXPD")), nil
 }
 
-func GrabValueFromHTMLName(name string, html *string) string {
-	return strings.ReplaceAll(strings.Split(strings.Split(strings.Split(*html, "csrf_token")[1], "/>")[0], "value=")[1], "\"", "")
+func GrabValueFromHTMLName(name string, html *[]byte) string {
+	return strings.ReplaceAll(strings.Split(strings.Split(strings.Split(string(*html), "csrf_token")[1], "/>")[0], "value=")[1], "\"", "")
 }
