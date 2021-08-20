0a09d4d461a221ed6774f449b705fde9647cecde
Kiwi <agiannoulis@mavestro.co>
2021-08-20 11:43:51 +0300

    Add runtime hook to task

--- go.mod
+++ go.mod
@@ -3,17 +3,15 @@ module github.com/ProjectAthenaa/sonic-core
 go 1.16
 
 require (
-	entgo.io/ent v0.8.0
+	entgo.io/ent v0.9.1
 	github.com/getsentry/sentry-go v0.11.0
 	github.com/go-redis/redis/v8 v8.11.0
-	github.com/google/uuid v1.2.0
+	github.com/google/uuid v1.3.0
 	github.com/grpc-ecosystem/go-grpc-middleware v1.0.0
 	github.com/json-iterator/go v1.1.10
 	github.com/lib/pq v1.10.2
-	github.com/otiai10/copy v1.6.0
 	github.com/prometheus/common v0.4.0
 	github.com/thoas/go-funk v0.9.0
-	github.com/urfave/cli v1.22.5
 	github.com/useflyent/fhttp v0.0.0-20210801005649-f160dd923789
 	github.com/viney-shih/go-lock v1.1.1
 	google.golang.org/grpc v1.39.0
--- sonic/database/connect.go
+++ sonic/database/connect.go
@@ -1,9 +1,14 @@
 package database
 
 import (
+	"context"
 	"entgo.io/ent/dialect"
 	"github.com/ProjectAthenaa/sonic-core/sonic/database/ent"
+	"github.com/ProjectAthenaa/sonic-core/sonic/database/ent/hook"
+	_ "github.com/ProjectAthenaa/sonic-core/sonic/database/ent/runtime"
+	"github.com/go-redis/redis/v8"
 	_ "github.com/lib/pq"
+	"os"
 )
 
 func Connect(pgURL string) *ent.Client {
@@ -11,9 +16,29 @@ func Connect(pgURL string) *ent.Client {
 	if err != nil {
 		panic(err)
 	}
+
+	client.Use(
+		func(next ent.Mutator) ent.Mutator {
+			return hook.TaskFunc(func(ctx context.Context, mutation *ent.TaskMutation) (ent.Value, error) {
+				if id, ok := mutation.ID(); ok {
+					rdb.Publish(ctx, "scheduler:tasks-deleted", id.String())
+				}
+
+				return next.Mutate(ctx, mutation)
+			})
+		})
+
 	//err = client.Schema.Create(context.Background())
 	//if err != nil {
 	//	panic(err)
 	//}
 	return client
 }
+
+var rdb = func() *redis.Client {
+	opts, err := redis.ParseURL(os.Getenv("REDIS_URL"))
+	if err != nil {
+		panic(err)
+	}
+	return redis.NewClient(opts)
+}()
