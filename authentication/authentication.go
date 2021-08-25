8bab1bb8232eefc65d699fe80daaf8fc43d4c6c2
Kiwi <agiannoulis@mavestro.co>
2021-08-25 15:58:52 +0300

    remove stupid shit

--- authentication/authentication.go
+++ authentication/authentication.go
@@ -59,9 +59,7 @@ func GenGraphQLAuthenticationFunc(base face.ICoreContext, graphEndpoint string, 
 
 	return func() gin.HandlerFunc {
 		return func(c *gin.Context) {
-			log.Info(c.Request.RemoteAddr)
-			//log.Info(c.Request.)
-			ip := c.Request.Header.Get("cf-connecting-ip")
+			ip := c.Request.Header.Get("x-original-forwarded-for")
 			ctx := context.WithValue(c.Request.Context(), "IP", ip)
 			ctx = context.WithValue(ctx, "Location", c.Request.Header.Get("cf-ipcountry"))
 			span := sentry.StartSpan(c.Request.Context(), "Authentication Middleware", sentry.TransactionName("Authentication"))
