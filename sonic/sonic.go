8c91bfe0dd76d54e505261893b7e4746fb5da2bd
Kiwi <agiannoulis@mavestro.co>
2021-08-31 11:37:19 +0300

    shape client

--- sonic/sonic.go
+++ sonic/sonic.go
@@ -5,12 +5,12 @@ import (
 	"github.com/ProjectAthenaa/sonic-core/sonic/antibots/shape"
 )
 
-//Is a wrapper for the internal NewClient method of PerimeterX
+// NewPerimeterXClient Is a wrapper for the internal NewClient method of PerimeterX
 func NewPerimeterXClient(svcURL ...string) (perimeterx.PerimeterXClient, error) {
 	return perimeterx.NewClient(svcURL...)
 }
 
-//Is a wrapper for the internal NewClient method of Shape
+// NewShapeClient Is a wrapper for the internal NewClient method of Shape
 func NewShapeClient(svcURL ...string) (shape.ShapeClient, error) {
 	return shape.NewClient(svcURL...)
 }
